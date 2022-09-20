4cb5e8af0e1fe38e6b90afed48ac6c74e59951c2
N.SAI BALAJI <112822934+NSAIBALAJI@users.noreply.github.com>
2022-09-20 22:37:20 +0530

    Create to convert Decimal number equivalent to Binary number and octal numbers.java

---
+++ to convert Decimal number equivalent to Binary number and octal numbers.java
@@ -0,0 +1,23 @@
+public class DecimalToBinary
+{
+  public static void main(String[] args)
+  {
+    int num = 14, tmp;
+    tmp = num;
+    String binary = "";
+    while(num >= 2)
+    {
+      binary = num%2 + binary ;
+      num = num /2;
+    }
+    binary = num + binary ;
+    System.out.println("Binary of " + tmp + " is " + binary);
+    while(num >= 8)
+    {
+      octal = num%8 + octal;
+      num = num /8;
+    }
+    octal = num + octal ;
+    System.out.println("Octal of " + tmp + " is " + octal);
+  }
+}
